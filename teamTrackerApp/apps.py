88b9b2a8f909de52078bd129f03c3543ba8bc523
Oladipo Osinubi <diposinubi@gmail.com>
2019-10-03 11:20:52 -0400

    inital commit

---
+++ teamTrackerApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TeamtrackerappConfig(AppConfig):
+    name = 'teamTrackerApp'
---
+++ teamTrackerApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+# # router.register(r'team', views.IssueItemViewSet)
+# router.register(r'user', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
