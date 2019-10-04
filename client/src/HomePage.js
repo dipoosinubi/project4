2ce51400c54a1b457676a98d45ad71cabba7a29d
Oladipo Osinubi <diposinubi@gmail.com>
2019-10-04 11:50:58 -0400

    created homepage route

---
+++ client/src/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class HomePage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World: This is Me</h1>
+            </div>
+        )
+    }
+}
