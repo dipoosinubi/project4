8c8b4472811bcb1152e8fd9a7bbea0ce3f5e5789
Oladipo Osinubi <diposinubi@gmail.com>
2019-10-07 11:47:40 -0400

    got merch by team id

--- client/src/components/TeamPage.js
+++ client/src/components/TeamPage.js
@@ -1,50 +1,38 @@
 import React, { Component } from 'react';
 
 
-
 export default class TeamPage extends React.Component {
     state = {
-        team: {},
-    //  merchandise: [],
+        team: {
+            merchandise: []
+        },
     };
     componentDidMount() {
-        this.getTeamAndMerchandise()
+       this.getTeam()
+        
     }
 
     getTeam = () => {
         fetch(`/api/team/${this.props.match.params.id}/`)
             .then(res => res.json())
+            .then(json => {
+                this.setState ({ team : json })
+            })
     }
-      getMerchandise = () => {
-          fetch(`/api/merchandise/`)
-          .then( res => res.json ())
-      }
-    
-    objectFromListById = (team, merchandise) => 
-    team.reduce((obj, team) => {
-        team.merchandise = merchandise.filter( merchandise => merchandise.team === team.id)
-        obj[team.id] = team
-        return obj;
-    }, {})
-
-    getTeamAndMerchandise = () => 
-      getTeam().then( team =>
-        getMerchandise().then( merchandise =>
-            objectFromListById(team, merchandise)
-            ))
+  
     render = () => {
         return (
             <div>
                 <h1>Team Page of {this.props.match.params.id}</h1>
                 Welcome to {this.state.team.name} Page
                 <div>
-                <ul>
-                    {this.state.team.merchandise.map(merchandise => (
-                        <li>
-                            {merchandise.description}
-                        </li>
-                    ))}
-                </ul>
+                    <ul>
+                        {this.state.team.merchandise.map(merchandise => (
+                            <li>
+                                {merchandise.description}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         )
--- teamTrackerApp/serializers.py
+++ teamTrackerApp/serializers.py
@@ -1,17 +1,23 @@
 from rest_framework import serializers
 from .models import Team, Merchandise, Schedule
 
-class TeamSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model   = Team
-        fields  = ['id', 'name', 'picture', 'website']
 
 class MerchandiseSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model    = Merchandise
-        fields   = ['id','description', 'picture', 'price', 'availability','website','team' ]
+    class Meta:
+        model = Merchandise
+        fields = ['id', 'description', 'picture',
+                  'price', 'availability', 'website', 'team']
+
 
 class ScheduleSerializer(serializers.ModelSerializer):
-    class  Meta:
-        model  = Schedule
-        fields  = ['id', 'name', 'date', 'time','location', 'website','team']
+    class Meta:
+        model = Schedule
+        fields = ['id', 'name', 'date', 'time', 'location', 'website', 'team']
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    schedule = ScheduleSerializer(many=True, read_only=True)
+    merchandise = MerchandiseSerializer(many=True, read_only=True)
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'picture', 'website', 'merchandise', 'schedule']
